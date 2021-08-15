99be6b92d7b6b642fae317cb7ddce5ea7b4b5f41
Vincent Garrigues <vincent@garriguv.io>
2021-08-15 13:25:29 +0200

    Add more dotfiles and brewfile

---
+++ dot_alias.zsh
@@ -0,0 +1,7 @@
+# ls
+alias l="ls -Flh"
+alias la="ls -FAlh"
+alias lsregister="/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister"
+
+# ruby
+alias be="bundle exec"
