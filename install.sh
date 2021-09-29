c5ccc9dd40108b5fc483326c143d81c1d1b956ec
Vincent Garrigues <vincent@garriguv.io>
2021-09-29 11:51:41 +0200

    Try to setup Codespaces all in install.sh

--- install.sh
+++ install.sh
@@ -19,5 +19,14 @@ fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+
+"$chezmoi" init --apply "--source=$script_dir"
+
+# https://github.com/codespaces-examples/base/blob/main/.devcontainer/setup.sh
+
+## setup and install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
